cd4fa990bb14998c01d4f3f12c24581f59dc84ed
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-11-19 11:24:05 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+//==================FUNCTION==============
+
+//Question 1
+
+// var now = new Date()
+// console.log(now)
+
+//Question 2
+
+// var fName = prompt("Enter Your First name").toLowerCase()
+// var lName = prompt("Enter Your last name").toLowerCase()
+
+// function fullName(fName, lName) {
+//     alert(fName + " " + lName)
+// }
+// fullName(fName, lName)
+
+//Question 3
+
+
+// var num1 = +prompt("Enter First Num");
+// var ope = prompt("Enter + ");
+// var num2 = +prompt("Enter second Num");
+
+// function add(num1, num) {
+//     if (ope === "+") {
+//         document.write(num1 + num2)
+//     } else
+//         document.write("Enter correct operator")
+
+// }
+// add(num1, num2)
+
+
+//Question 4
+
+// var num1 = +prompt("Enter First Num");
+// var ope = prompt("Enter operator ");
+// var num2 = +prompt("Enter second Num");
+
+// function add(num1, num) {
+//     if (ope === "+") {
+//         document.write(num1 + num2)
+//     } else if (ope === "-") {
+//         document.write(num1 - num2)
+//     } else if (ope === "*") {
+//         document.write(num1 * num2)
+//     } else if (ope === "/") {
+//         document.write(num1 / num2)
+//     } else
+//         document.write("Enter correct operator")
+
+// }
+// add(num1, num2)
+
+//Question 5
+// var a = +prompt("Enter a Number")
+
+// function squ(a) {
+//     document.write("The Square Of " + " " + a + " Is: " + a * a)
+// }
+// squ(a)
+
+///Question 7
+
+// var a = +prompt("Enter First Num")
+// var b = +prompt("Enter First second")
+
+
+// function c() {
+//     for (var i = a; i <= b; i++)
+//         document.write("<br>" + i)
+// }
+// c()
+
+
+//Question 8
+
+
+// var a = +prompt("Enter Perpendicular")
+// var b = +prompt("Enter Base")
+
+
+// function c() {
+//     var d = a + b
+//     var e = d * d
+//     document.write("The Hypotenuse Is: " + d)
+//     document.write("<br>The Square Of Hypotenuse Is: " + e)
+
+// }
+// c()
+
+
+//Question 9
+
+
+// var a = +prompt("Enter Width Lenght")
+// var b = +prompt("Enter Height Lenght")
+
+// function c() {
+//     var d = a * b;
+//     document.write("The Area Of Rectangle Is: " + d)
+// }
+// c()
